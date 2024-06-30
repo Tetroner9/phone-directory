66139bf811f4406b201dc880728971a779ba3935
Tetroner9 <omkadam2125@gmail.com>
2024-06-30 19:34:32 +0530

    Created ‘Header’ Component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
-import React, { Fragment } from 'react';
+import React, { Component } from 'react';
+import Header from './Header.js';
 
-function App() {
-  return (
-    // <div>
-    //   <div className="Header">
-    //     Phone Directory
-    //   </div>
-    //   <button>Add</button>
-    //   <div>
-    //     <span>Name</span><br/>
-    //     <span>Phone</span>
-    //   </div>
-    // </div>
-
-    <Fragment>
+class App extends Component {
+  render() {
+    return (
       <div>
-         Phone Directory
+        <Header/>
+        <button>Add</button>
+        <div>
+          <span>Name</span><br />
+          <span>Phone</span>
+        </div>
       </div>
-      <button>Add</button>
-      <div>
-         <span>Name</span>
-         <span>Phone</span>
-      </div>
-    </Fragment>
-  );
+    );
+  }
 }
 
 export default App;
